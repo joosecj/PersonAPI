57b5163b661f3117849b512e8d6fcec3c4a0236a
Jose Carlos <josecarloscjj@gmail.com>
2022-10-13 21:44:48 -0300

    Creat Person entity

---
+++ backend/src/main/java/com/person/api/entities/Person.java
@@ -0,0 +1,60 @@
+package com.person.api.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+
+    private LocalDate birthDate;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, String email, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
