30ac2451c3a8a411667ba6303648e0891da34889
Jose Carlos <josecarloscjj@gmail.com>
2022-10-13 21:55:31 -0300

    Seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_person(name, email, birth_date) VALUES ( 'Clara Maria', 'clara@gmail.com', '1990-03-17');
+INSERT INTO tb_person(name, email, birth_date) VALUES ( 'Antonio José', 'antoniojs@gmail.com', '1983-05-01');
+INSERT INTO tb_person(name, email, birth_date) VALUES ( 'Rita de Cassia', 'ritacassia@gmail.com', '1994-12-05');
+INSERT INTO tb_person(name, email, birth_date) VALUES ( 'Carlos Alberto', 'albertocarlos@gmail.com', '2000-08-25');
