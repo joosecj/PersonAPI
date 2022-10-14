d5056e33d8198acaa457e493343ac47318a8d4c3
Jose Carlos <josecarloscjj@gmail.com>
2022-10-13 23:05:27 -0300

    findAll Person

--- backend/src/main/java/com/person/api/controllers/PersonController.java
+++ backend/src/main/java/com/person/api/controllers/PersonController.java
@@ -1,8 +1,12 @@
 package com.person.api.controllers;
 
 import com.person.api.DTO.PersonDTO;
+import com.person.api.entities.Person;
 import com.person.api.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,4 +22,10 @@ public class PersonController {
     public PersonDTO findById(@PathVariable Long id) {
         return personService.findById(id);
     }
+
+    @GetMapping
+    public Page<PersonDTO> findAll (Pageable pageable) {
+        return personService.findAll(pageable);
+    }
+
 }
--- backend/src/main/java/com/person/api/services/PersonService.java
+++ backend/src/main/java/com/person/api/services/PersonService.java
@@ -4,15 +4,26 @@ import com.person.api.DTO.PersonDTO;
 import com.person.api.entities.Person;
 import com.person.api.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Transactional(readOnly = true)
     public PersonDTO findById(Long id) {
         Person personEntity = personRepository.findById(id).get();
         return new PersonDTO(personEntity);
     }
+
+    @Transactional(readOnly = true)
+    public Page<PersonDTO> findAll(Pageable pageable) {
+        Page<Person> people = personRepository.findAll(pageable);
+        return people.map(PersonDTO::new);
+    }
+
 }
