b6dc7cdf034e94148ba750cd3535b9d43de9cf78
Jose Carlos <josecarloscjj@gmail.com>
2022-10-13 21:21:55 -0300

    Security config

---
+++ backend/src/main/java/com/person/api/PersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.person.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/person/api/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
